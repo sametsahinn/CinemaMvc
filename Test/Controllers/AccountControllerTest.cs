b2ce5fed6c04e0410916649082364677471262c7
sametsahinn <samet.sahin.0122@hotmail.com>
2016-12-23 12:48:04 +0300

    Unit Test

---
+++ Test/Controllers/AccountControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using CinemaWeb.Controllers;
+
+namespace Test.Controllers
+{
+
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void LoginTest()
+        {
+            AccountController dd = new AccountController();
+            dd.Control("test", "Test"); //User Name & Pwd
+            ViewResult result = dd.Control("test", "Test") as ViewResult;
+            Assert.AreEqual("Hatalı kullanıcı adı veya parola !", result.TempData["error"]);
+        }
+    }
+}
---
+++ Test/Controllers/FilmControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web;
+using CinemaWeb.Controllers;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Web.Mvc;
+
+namespace Test.Controllers
+{
+    [TestClass]
+    public class FilmControllerTest
+    {
+        FormCollection collection; 
+        [TestMethod]
+        public void FilmAddTest()
+        {
+            FilmController dd = new FilmController();
+            dd.FilmDeleteAction(collection);   //FormCollection içinde FILMID değerini sakladığım button çinden verisini alıp sildiriyorum.
+            ViewResult result = dd.FilmDeleteAction(collection) as ViewResult;
+            Assert.AreEqual("Film Silindi.", result.TempData["success"]);
+        }
+    }
+}
